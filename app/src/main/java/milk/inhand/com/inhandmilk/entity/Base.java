0188ec961fa41b5f6b914d9ae83caa793604c3a8
yoyoyohamapi <472285740@qq.com>
2015-03-05 19:31:22 +0800

    Sync got

--- app/src/main/java/milk/inhand/com/inhandmilk/entity/Base.java
+++ app/src/main/java/milk/inhand/com/inhandmilk/entity/Base.java
@@ -1,13 +1,5 @@
 package milk.inhand.com.inhandmilk.entity;
 
-import android.content.Context;
-
-import com.alibaba.fastjson.JSON;
-
-import java.util.UUID;
-
-import milk.inhand.com.inhandmilk.helper.DatabaseHelper;
-
 /**
  * Base
  * Desc:实体基类，子类需要重载getTable()方法
@@ -17,33 +9,5 @@ import milk.inhand.com.inhandmilk.helper.DatabaseHelper;
  * Time: 16:06
  */
 public class Base {
-    protected DatabaseHelper dbHelper;
-    protected String localUuid;
-
-    public Base() {
-    }
-
-    public Base(Context ctx) {
-        this.dbHelper = new DatabaseHelper(ctx);
-        UUID uuid = UUID.randomUUID();
-        this.localUuid = uuid.toString();
-    }
-
-    public String getLocalUuid() {
-        return localUuid;
-    }
-
-    public String getTable(){
-        return null;
-    }
-
-    /**
-     * 存储数据对象至数据库
-     */
-    public void save(){
-        //转换为json存入sqlite
-        String obj = JSON.toJSONString(this);
-        this.dbHelper.insert(this.getTable(),obj,getLocalUuid());
-    }
 
 }
--- app/src/main/java/milk/inhand/com/inhandmilk/helper/LeanCloudHelper.java
+++ app/src/main/java/milk/inhand/com/inhandmilk/helper/LeanCloudHelper.java
@@ -1,10 +1,11 @@
 package milk.inhand.com.inhandmilk.helper;
 
-import android.app.Activity;
 import android.content.Context;
 
-import com.avos.avoscloud.AVAnalytics;
 import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.AVObject;
+
+import milk.inhand.com.inhandmilk.entity.OneDay;
 
 /**
  * LeanCloudHelper
@@ -15,9 +16,32 @@ import com.avos.avoscloud.AVOSCloud;
  * Time: 12:49
  */
 public class LeanCloudHelper {
-    public static  void initLeanCloud(Context ctx){
-        AVOSCloud.initialize(ctx, "hfmv1q7runm51bzbgkeu2brt5e2waawmr0519cag9g7dxxf2", "rfhukesd0eyobas5h00aeuzholiczwepkyios3q75jixoawh");
-        AVAnalytics.trackAppOpened( ((Activity)ctx).getIntent() );
+    /**
+     * 初始化LeanCloud配置
+     * @param ctx 上下文环境
+     */
+    public static void initLeanCloud(Context ctx){
+        // 注册子类
+        registerSubclass();
+        AVOSCloud.initialize(ctx, "5dwer2xd20x59apeyfuq2bv5scn14078k0bzwqk6lnho6417", "65z49j48dcr57bplz9ecqlp3b8n8eq30sbkhpdawilg0z5q7");
         AVOSCloud.setDebugLogEnabled(true);
+    }
+
+    private static void registerSubclass(){
+        AVObject.registerSubclass(OneDay.class);
+    }
+    /**
+     * 与leancloud数据同步
+     */
+    public static void sync(){
+
+    }
+
+    private void save(){
+
+    }
+
+    private void update(){
+
     }
 }
