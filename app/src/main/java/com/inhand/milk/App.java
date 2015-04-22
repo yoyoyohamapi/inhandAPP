be03e4236db056aa01043aa8b1adebf1d49ffaa1
yoyoyohamapi <472285740@qq.com>
2015-04-22 21:28:43 +0800

    Mixed OK!

--- app/src/main/java/com/inhand/milk/App.java
+++ app/src/main/java/com/inhand/milk/App.java
@@ -5,14 +5,9 @@ import android.app.Application;
 import com.alibaba.fastjson.JSON;
 import com.avos.avoscloud.AVUser;
 import com.inhand.milk.entity.Baby;
-import com.inhand.milk.entity.FooterItem;
-import com.inhand.milk.entity.SlidingItem;
 import com.inhand.milk.entity.User;
-import com.inhand.milk.helper.JSONHelper;
 import com.inhand.milk.helper.LeanCloudHelper;
 import com.inhand.milk.utils.ACache;
-
-import java.util.List;
 
 /**
  * App
@@ -23,40 +18,18 @@ import java.util.List;
  * Time: 10:37
  */
 public class App extends Application {
-    public static final String FOOTER_CONFIG = "config/footer_items.json";
-    public static final String SLIDING_CONFIG = "config/sliding_items.json";
     public static final String BABY_CACHE_KEY = "current_baby";
     public static Baby currentBaby = null;
-    private List<FooterItem> footerItems = null;
-    private List<SlidingItem> slidingItems = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
         //初始化LeanCloud
         LeanCloudHelper.initLeanCloud(this);
-        //初始化底部菜单项
-        slidingItems = JSONHelper.parseJSONArray(
-                this,
-                App.SLIDING_CONFIG,
-                SlidingItem.class
-        );
-        //初始化侧边栏
-        footerItems = JSONHelper.parseJSONArray(
-                this,
-                App.FOOTER_CONFIG,
-                FooterItem.class
-        );
         initCurrentBaby();
     }
 
-    public List<FooterItem> getFooterItems() {
-        return footerItems;
-    }
 
-    public List<SlidingItem> getSlidingItems() {
-        return slidingItems;
-    }
 
     public static User getCurrentUser() {
         return AVUser.cast(AVUser.getCurrentUser(), User.class);
